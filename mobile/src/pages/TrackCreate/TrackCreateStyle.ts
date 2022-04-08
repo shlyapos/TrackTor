db8d3a7e5e344745daafaa12fe83d856c30cdd7e
Alexander <alexandrsuchkov67@gmail.com>
2022-04-09 00:40:31 +0300

    Updated styles for pages

---
+++ mobile/src/pages/TrackCreate/TrackCreateStyle.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import Colors from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.colorBackground,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default styles;
