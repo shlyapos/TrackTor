4cdb2dc5f0bdc92e0eb6f0e079491d3b91a23ede
Alexander <alexandrsuchkov67@gmail.com>
2022-04-10 13:28:32 +0300

    Updated TrackListPage

--- mobile/src/pages/TrackList/TrackListPage.tsx
+++ mobile/src/pages/TrackList/TrackListPage.tsx
@@ -1,13 +1,44 @@
 import { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import TopMenu from '../../components/TopMenu';
+import { IFrontendTrack } from '../../models/tracks';
+import TrackListItem from './components/TrackListItem';
 
 import styles from './TrackListStyle';
 
-export default class TrackListPage extends PureComponent {
+interface ITrackListPageProps {
+  tracks: IFrontendTrack[];
+  searchValue: string;
+  onPress: (id: string) => void; // TODO: во время реализации навигации, поменять переменные
+  onChangeSearch: (value: string) => void;
+}
+
+export default class TrackListPage extends PureComponent<ITrackListPageProps> {
   render() {
+    const { tracks, searchValue, onChangeSearch, onPress } = this.props;
+
     return (
       <View style={styles.container}>
-        <Text>TrackListPage</Text>
+        <TopMenu
+          isSearch
+          searchValue={searchValue}
+          onChangeSearch={onChangeSearch}
+        />
+
+        <ScrollView style={styles.list}>
+          {tracks.map((track) => (
+            <TrackListItem
+              key={track.id}
+              id={track.id}
+              name={track.name}
+              region={track.region}
+              transport={track.transport}
+              distance={track.distance}
+              time={track.time}
+              onPress={onPress}
+            />
+          ))}
+        </ScrollView>
       </View>
     );
   }
--- mobile/src/pages/TrackList/TrackListStyle.ts
+++ mobile/src/pages/TrackList/TrackListStyle.ts
@@ -1,12 +1,20 @@
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 import { StyleSheet } from 'react-native';
 
 import Colors from '../../styles/colors';
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
     backgroundColor: Colors.colorBackground,
+    display: 'flex',
+    paddingTop: Platform.OS === 'ios' ? Constants.statusBarHeight : 0,
     width: '100%',
     height: '100%',
+  },
+  list: {
+    width: '100%',
   },
 });
 
