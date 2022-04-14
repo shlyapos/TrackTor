6327b36050202bf977149f1fd8ca7a3aff3dadc3
PSASP <andrey.psbk@gmail.com>
2022-04-14 13:55:56 +0300

    Initted result and user models and configurations. Updated dbcontext

---
+++ Backend/TrackTor/TrackTor.Database/Configurations/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TrackTor.DataBase.Models;
+
+namespace TrackTor.DataBase.Configurations
+{
+    public class UserConfiguration: IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasIndex(user => user.Id).IsUnique();
+            builder.HasKey(user => user.Id);
+            builder.Property(user => user.Id).IsRequired();
+
+            builder.Property(user => user.Login).IsRequired();
+            
+            builder.Property(user => user.Hash).IsRequired();
+            builder.Property(user => user.Salt).IsRequired();
+        }
+    }
+}
---
+++ Backend/TrackTor/TrackTor.Database/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrackTor.DataBase.Models
+{
+    public class User
+    {
+        public Guid Id { get; }
+        public string Login { get; set; }
+        public string Hash { get; set; }
+        public string Salt { get; set; }
+    }
+}
