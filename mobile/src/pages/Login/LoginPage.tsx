8c867ccbbd5c4e4aff32ac29bddde5f8797eed00
Alexander <alexandrsuchkov67@gmail.com>
2022-04-15 13:16:33 +0300

    Added empty login page for tab navigator

---
+++ mobile/src/pages/Login/LoginPage.tsx
@@ -0,0 +1,8 @@
+import { PureComponent } from 'react';
+import { Text } from 'react-native';
+
+export default class LoginPage extends PureComponent {
+  render() {
+    return <Text>LoginPage</Text>;
+  }
+}
