334e275ce75d6fc948999efabab795c1d495c367
Alexander <alexandrsuchkov67@gmail.com>
2022-04-20 23:18:41 +0300

    Added page for track create

--- mobile/src/pages/TrackCreate/TrackCreatePage.tsx
+++ mobile/src/pages/TrackCreate/TrackCreatePage.tsx
@@ -1,13 +1,51 @@
 import { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import TopMenu from '../../components/TopMenu';
+import { Transport } from '../../models/tracks';
+import { StyleSheet } from 'react-native';
 
 import styles from './TrackCreateStyle';
 
-export default class TrackCreatePage extends PureComponent {
+interface ITrackCreatePageProps {
+  transports: Transport[];
+  onPressBack: () => void;
+  onChangeName: (value: string) => void;
+  onChangeTransport: (value: Transport) => void;
+  onPressStart: () => void;
+}
+
+export default class TrackCreatePage extends PureComponent<ITrackCreatePageProps> {
   render() {
+    const { onChangeName, onPressStart } = this.props;
+
     return (
       <View style={styles.container}>
-        <Text>TrackCreatePage</Text>
+        <TopMenu onPress={() => {}} />
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            onChange={() => onChangeName}
+            autoComplete={'off'}
+            placeholder='Название'
+            mode={'outlined'}
+            style={styles.input}
+            outlineColor={styles.inputOutLine.color}
+            activeOutlineColor={styles.inputOutLine.color}
+            placeholderTextColor={styles.inputPlaceholder.color}
+            selectionColor={styles.inputSelection.color}
+            theme={{ colors: { text: styles.input.color } }}
+          />
+          <Button
+            contentStyle={styles.buttonContent}
+            labelStyle={styles.buttonLabel}
+            mode='contained'
+            onPress={onPressStart}
+            theme={{ colors: { background: styles.buttonLabel.color } }}
+          >
+            старт
+          </Button>
+        </View>
       </View>
     );
   }
--- mobile/src/pages/TrackCreate/index.tsx
+++ mobile/src/pages/TrackCreate/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import TrackCreatePage from './TrackCreatePage';
 
+import { Transport } from '../../models/tracks';
+
 const TrackCreate = () => {
+  const [name, setName] = React.useState('');
+  const [transport, setTransport] = React.useState<Transport>();
+
+  const onPressBack = () => {};
+
+  const onChangeName = (value: string) => setName(value);
+
+  const onChangeTransport = (value: Transport) => setTransport(value);
+
+  const onPressStart = () => {};
+
   return (
-    <TrackCreatePage />
+    <TrackCreatePage
+      onChangeName={onChangeName}
+      onChangeTransport={onChangeTransport}
+      onPressBack={onPressBack}
+      onPressStart={onPressStart}
+    />
   );
 };
 
