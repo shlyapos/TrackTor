313deef21595b8f6f12fbd510e7f3b38af97d5f2
nikita <nikita.garasev@qoollo.com>
2022-04-21 13:27:06 +0300

    [49] minor fix

--- Backend/TrackTor/TrackTor.Database/Models/TrackCheckPoint.cs
+++ Backend/TrackTor/TrackTor.Database/Models/TrackCheckPoint.cs
@@ -6,7 +6,7 @@ namespace TrackTor.DataBase.Models
     {
         public Guid Id { get; }
         public Guid TrackId { get; set; }
-        public string Longitude { get; set; }
-        public string Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
