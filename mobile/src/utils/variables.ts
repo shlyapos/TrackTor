e0792295377d965c053dd8bfdd851ea62994c0bb
Alexander <alexandrsuchkov67@gmail.com>
2022-04-23 23:52:21 +0300

    Added variable for map zoom on user

---
+++ mobile/src/utils/variables.ts
@@ -0,0 +1,10 @@
+export const boundingBox = {
+  southWest: {
+    latitude: 24.234631,
+    longitude: 89.907127,
+  },
+  northEast: {
+    latitude: 24.259769,
+    longitude: 89.934692,
+  },
+};
