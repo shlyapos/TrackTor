099f48089b497ee34a6a27e38da965b5e2af89d5
Alexander <alexandrsuchkov67@gmail.com>
2022-04-24 23:44:18 +0300

    Updated login route types

--- mobile/src/stack/RootStackNavigator.tsx
+++ mobile/src/stack/RootStackNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../pages/Login';
 import TrackInfo from '../pages/TrackInfo';
 import TrackRecord from '../pages/TrackRecord';
 import TabNavigator from '../components/TabNavigator';
@@ -9,8 +10,12 @@ const RootStack = createStackNavigator<RootStackParams>();
 
 const RootStackNavigator: React.FC = () => {
   return (
-    <RootStack.Navigator screenOptions={{ headerShown: false }}>
+    <RootStack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName={'Login'}
+    >
       <RootStack.Screen name='Home' component={TabNavigator} />
+      <RootStack.Screen name='Login' component={Login} />
       <RootStack.Screen name='TrackInfo' component={TrackInfo} />
       <RootStack.Screen name='TrackRecord' component={TrackRecord} />
     </RootStack.Navigator>
