177054de7878a997e5ac5d0073f84f28dc3a26a8
Kulikov <dakulikov2001@mail.ru>
2022-04-24 20:55:29 +0300

    add auth

--- web-client/src/app/auth/services/auth.service.ts
+++ web-client/src/app/auth/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { TuiAlertService, TuiNotification } from '@taiga-ui/core';
+import { BehaviorSubject, EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { User } from 'src/app/shared/models/user';
 
+
+export const ERROR_LABEL_AUTH = 'Не удалось авторизоваться';
+export const ERROR_LABEL_REGISTRATION = 'Не удалось зарегистрироваться';
+export const ERROR_MESSAGE_AUTH = 'Ошибка сервера или проверьте логин и пароль';
+export const ERROR_MESSAGE_REGISTRATION = 'Ошибка сервера или пользователь с таким логином уже существует';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +19,12 @@ export class AuthService {
   private currentUserSubject: BehaviorSubject<User | null>;
   public currentUser: Observable<User | null>;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private readonly http: HttpClient,
+    @Inject(TuiAlertService)
+    private readonly alertService: TuiAlertService,
+    private readonly router: Router,
+  ) {
     const user = JSON.parse(JSON.stringify(localStorage.getItem('currentUser')));
 
     this.currentUserSubject = new BehaviorSubject<User | null>(user ? {
@@ -30,13 +42,38 @@ export class AuthService {
     return this.currentUserSubject.value ? true : false;
   }
 
-  login(username: string, password: string) {
-    return this.http.post<any>('/users/auth', { username, password })
+  signIn(login: string, password: string) {
+    return this.http.post<User>('/auth', { login, password })
       .pipe(
         map(user => {
           localStorage.setItem('currentUser', JSON.stringify(user));
           this.currentUserSubject.next(user);
           return user;
+        }),
+        catchError((error) => {
+          this.alertService.open(`${ERROR_MESSAGE_AUTH}: ${error}`, {
+            label: ERROR_LABEL_AUTH,
+            status: TuiNotification.Error,
+          }).subscribe();
+          return EMPTY;
+        }),
+      );
+  }
+
+  registration(login: string, password: string) {
+    return this.http.post<User>('/registration', { login, password })
+      .pipe(
+        map(user => {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
+        }),
+        catchError((error) => {
+          this.alertService.open(`${ERROR_MESSAGE_REGISTRATION}: ${error}`, {
+            label: ERROR_LABEL_REGISTRATION,
+            status: TuiNotification.Error,
+          }).subscribe();
+          return EMPTY;
         }),
       );
   }
@@ -44,5 +81,6 @@ export class AuthService {
   logout() {
     localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
+    this.router.navigate(['auth']);
   }
 }
