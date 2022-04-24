a42cd64b30369d0b5edae9e453ea0bb74f16b993
Alexander <alexandrsuchkov67@gmail.com>
2022-04-24 23:45:31 +0300

    Added login page

--- mobile/src/pages/Login/LoginPage.tsx
+++ mobile/src/pages/Login/LoginPage.tsx
@@ -1,8 +1,80 @@
 import { PureComponent } from 'react';
-import { Text } from 'react-native';
+import {
+  View,
+  ImageBackground,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import backgroundImage from '../../assets/background.png';
+import ActionBlock from './components/ActionBlock';
+import MainIcon from './components/MainIcon';
 
-export default class LoginPage extends PureComponent {
+import styles from './LoginPageStyle';
+
+interface ILoginPageProps {
+  loginSignIn: string;
+  passwordSignIn: string;
+  loginSignUp: string;
+  passwordSignUp: string;
+  onChangeLoginSignIn: (value: string) => void;
+  onChangePasswordSignIn: (value: string) => void;
+  onPressSignIn: () => void;
+  onChangeLoginSignUp: (value: string) => void;
+  onChangePasswordSignUp: (value: string) => void;
+  onPressSignUp: () => void;
+}
+
+const OS = Platform.OS;
+
+export default class LoginPage extends PureComponent<ILoginPageProps> {
   render() {
-    return <Text>LoginPage</Text>;
+    const {
+      loginSignIn,
+      passwordSignIn,
+      loginSignUp,
+      passwordSignUp,
+      onChangeLoginSignIn,
+      onChangePasswordSignIn,
+      onPressSignIn,
+      onChangeLoginSignUp,
+      onChangePasswordSignUp,
+      onPressSignUp,
+    } = this.props;
+    return (
+      <KeyboardAvoidingView
+        enabled={false}
+        behavior={OS === 'ios' ? 'padding' : 'height'}
+        // style={styles.container}
+      >
+        <ImageBackground source={backgroundImage} style={styles.container}>
+          <View style={styles.iconContainer}>
+            <MainIcon />
+          </View>
+
+          <ScrollView contentContainerStyle={styles.containerScroll}>
+            <ActionBlock
+              title='Войти'
+              valueLogin={loginSignIn}
+              valuePassword={passwordSignIn}
+              onChangeLogin={onChangeLoginSignIn}
+              onChangePassword={onChangePasswordSignIn}
+              onPress={onPressSignIn}
+            />
+
+            <View style={styles.separator}></View>
+
+            <ActionBlock
+              title='Регистрация'
+              valueLogin={loginSignUp}
+              valuePassword={passwordSignUp}
+              onChangeLogin={onChangeLoginSignUp}
+              onChangePassword={onChangePasswordSignUp}
+              onPress={onPressSignUp}
+            />
+          </ScrollView>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
   }
 }
--- mobile/src/pages/Login/index.tsx
+++ mobile/src/pages/Login/index.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import LoginPage from './LoginPage';
+import { LoginScreenNavigationProp } from '../../stack';
 
-const Login: React.FC = () => {
-  return <LoginPage />;
+interface ILoginPageProps {
+  navigation: LoginScreenNavigationProp;
+}
+
+const Login: React.FC<ILoginPageProps> = ({ navigation }) => {
+  const [loginSignIn, setLoginSignIn] = React.useState('');
+  const [passwordSignIn, setPasswordSignIn] = React.useState('');
+
+  const [loginSignUp, setLoginSignUp] = React.useState('');
+  const [passwordSignUp, setPasswordSignUp] = React.useState('');
+
+  const onChangeLoginSignIn = (value: string) => setLoginSignIn(value);
+  const onChangePasswordSignIn = (value: string) => setPasswordSignIn(value);
+  const onPressSignIn = () => {
+    navigation.navigate('Home');
+  };
+
+  const onChangeLoginSignUp = (value: string) => setLoginSignUp(value);
+  const onChangePasswordSignUp = (value: string) => setPasswordSignUp(value);
+  const onPressSignUp = () => {
+    navigation.navigate('Home');
+  };
+
+  return (
+    <LoginPage
+      loginSignIn={loginSignIn}
+      passwordSignIn={passwordSignIn}
+      loginSignUp={loginSignUp}
+      passwordSignUp={passwordSignUp}
+      onChangeLoginSignIn={onChangeLoginSignIn}
+      onChangePasswordSignIn={onChangePasswordSignIn}
+      onPressSignIn={onPressSignIn}
+      onChangeLoginSignUp={onChangeLoginSignUp}
+      onChangePasswordSignUp={onChangePasswordSignUp}
+      onPressSignUp={onPressSignUp}
+    />
+  );
 };
 
 export default Login;
