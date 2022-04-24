da5301286dca515b2b2fc91a31922847eb1dced5
nikita <nikita.garasev@qoollo.com>
2022-04-24 23:41:59 +0300

    [46] fix

--- Backend/TrackTor/TrackTor.Adaptor/Repositories/ProviderExtension.cs
+++ Backend/TrackTor/TrackTor.Adaptor/Repositories/ProviderExtension.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using TrackTor.Adaptor.Api;
+using TrackTor.Adaptor.Repositories;
 using TrackTor.Repositories.Api;
 
 namespace TrackTor.Repositories.Implementation
@@ -7,6 +9,9 @@ namespace TrackTor.Repositories.Implementation
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
+            services.AddTransient<IResultRepository, ResultRepository>();
+            services.AddTransient<ITrackCheckPointRepository, TrackCheckPointRepository>();
+            services.AddTransient<ITrackRepository, TrackRepository>();
             services.AddTransient<IUserRepository, UserRepository>();
             return services;
         }
